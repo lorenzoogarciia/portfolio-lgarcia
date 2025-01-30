aaf419b716310a6c7cee46b7744b5ee212dc732d
lorenzoogarciia_ <lorenzogarciagarcia03@gmail.com>
2025-01-30 19:59:11 +0100

    Title structure - study card

--- app/ui/home/study-card.tsx
+++ app/ui/home/study-card.tsx
@@ -14,7 +14,7 @@ export default function StudyCard(
             <div className="rounded-xl border-2 border-secondary p-4 max-w-[300px] max-h-[200px] transition-colors duration-150 hover:border-primary hover:bg-secondary hover:text-primary">
                 <h1 className="text-xl">{title}</h1>
                     <h3 className="text-lg">{center}</h3>
-                    <h2 className="text-lg font-normal">{date}</h2>
+                    <h2 className="text-base font-normal">{date}</h2>
             </div>
         </div>
     )
