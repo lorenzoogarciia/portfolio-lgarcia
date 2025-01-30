7df3ec2c1a0a8af60b1feba663a4818e1855a1a5
lorenzoogarciia_ <lorenzogarciagarcia03@gmail.com>
2025-01-30 13:03:01 +0100

    Metadata for Dinamic Routes

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -7,9 +7,15 @@ interface LayoutProps {
 }
 
 export async function generateMetadata({ params }: LayoutProps): Promise<Metadata> {
+    if (!params || !params.slug) {
+        return {
+            title: "404 Not Found | LGarciaDev",
+            description: "No se encontró la información del proyecto.",
+        };
+    }
     const project = await getProjectBySlug(params.slug)
     return {
-        title: `${project?.title} | LGarciaDev`,
+        title: `${project?.title} | LGarciaDev` || "404 Not Found",
         description: `${project?.description}` || "Descripción del proyecto no disponible",
     }
 }
