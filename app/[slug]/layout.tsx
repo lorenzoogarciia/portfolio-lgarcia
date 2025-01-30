8747e3ce0a1d64635f190e10ed76ea1b60975ff4
lorenzoogarciia_ <lorenzogarciagarcia03@gmail.com>
2025-01-30 13:24:05 +0100

    Improved metadata

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -3,17 +3,11 @@ import { getProjectBySlug } from "../lib/actions";
 
 interface LayoutProps {
     children: React.ReactNode;
-    params: {slug: string};
+    slug: string;
 }
 
-export async function generateMetadata({ params }: LayoutProps): Promise<Metadata> {
-    if (!params || !params.slug) {
-        return {
-            title: "404 Not Found | LGarciaDev",
-            description: "No se encontró la información del proyecto.",
-        };
-    }
-    const project = await getProjectBySlug(params.slug)
+export async function generateMetadata({ slug }: LayoutProps): Promise<Metadata> {
+    const project = await getProjectBySlug(slug)
     return {
         title: `${project?.title} | LGarciaDev` || "404 Not Found",
         description: `${project?.description}` || "Descripción del proyecto no disponible",
